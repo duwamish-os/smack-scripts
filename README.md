f03ff00bf22a9d3749a44d5cc050e8de5c286608
Upadhyay <prayag.upd@gmail.com>
2018-02-24 15:54:27 -0800

    experiment with echo with " and add tail -f on tomcat container logs

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 smack windows
 -------------
 
+start a window
 
+```
+./convo.sh
+```
+
+exit the window
+
+```
+C-b :detach
+tmux kill-session -t convo
+```
 
--- convo.sh
+++ convo.sh
@@ -4,9 +4,9 @@ tmux new-session -d -s convo ;
 tmux split-window -h ;
 
 tmux select-pane -L
-tmux send-keys -t convo 'echo hi' C-m
+tmux send-keys -t convo 'echo "Hi how can I help you?"' C-m
 
 tmux select-pane -R
-tmux send-keys -t convo 'top' C-m
+tmux send-keys -t convo 'tail -f /usr/local/apache-tomcat-8.5.12/logs/catalina.out' C-m
 
 tmux attach-session -d -t convo
