1989f55cd2c3ed298ed2f1bcb228762f65984302
Upadhyay <prayag.upd@gmail.com>
2018-02-25 09:03:06 -0800

    add sshing the docker container and tailing log files

--- initialise.sh
+++ initialise.sh
@@ -8,9 +8,18 @@ function conversation-init-tmux {
   tmux rename-window 'Conversation client'
 
   tmux select-pane -U
+  printf '\033]2;Conversation logs\033\\'
+  tmux set -g pane-border-format "#{pane_index} #{pane_current_command}"
   tmux send-keys -t conversation-session 'echo "Starting conversation..."' C-m
   tmux send-keys -t conversation-session 'echo "Bot: Hi, how can I help you, $1"' C-m
 
+  tmux send-keys -t conversation-session "docker_container=$(docker ps -aqf name=convo-api)" 'C-m' 'docker exec -it $docker_container bash' 'C-m'
+  tmux send-keys -t conversation-session 'tail -f logs.dir_IS_UNDEFINED/application.log' C-m
+ 
+  tmux select-pane -D
+  tmux send-keys -t conversation-session "docker_container=$(docker ps -aqf name=convo-api)" 'C-m' 'docker exec -it $docker_container bash' 'C-m'
+  tmux send-keys -t conversation-session 'tail -f logs/catalina*' C-m
+  
   tmux attach-session -d -t conversation-session
 }
 
